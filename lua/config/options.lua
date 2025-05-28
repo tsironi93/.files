859f5c1502e70773b6b9921d1abbad141f8a4ad1
Ioannis Tsiros <itsiros@2-G-8.42heilbronn.de>
2025-05-28 11:41:53 +0200

    clean

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -24,7 +24,7 @@ vim.g.lazyvim_picker = "auto"
 -- Can be one of: nvim-cmp, blink.cmp
 -- Leave it to "auto" to automatically use the completion engine
 -- enabled with `:LazyExtras`
-vim.g.lazyvim_cmp = "auto"
+vim.g.lazyvim_cmp = "blink.cmp"
 
 -- if the completion engine supports the AI source,
 -- use that instead of inline suggestions
